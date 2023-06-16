85a8d9bcb193f783a1df34da61f5e722d6617036
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-16 09:54:36 +0430

    ADD TABLES AND FOREIGN KEYS

---
+++ db/migrate/20230619050050_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :note
+      t.text :image
+      t.belongs_to :list, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
