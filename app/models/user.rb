bc96821035daf097f8b9ba7ccd797604ea350c43
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-26 06:13:51 +0430

    1- Update Validations in User model 2- Fix Sign out issue 3- Add default url options 4- Set rack cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-    has_many :lists
-    has_one :active_list, -> { where(active: true) }, class_name: 'List'
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
-    validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+  has_many :lists
+  has_one :active_list, -> { where(active: true) }, class_name: 'List'
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+
+  def jwt_payload
+    super
+  end
 end
