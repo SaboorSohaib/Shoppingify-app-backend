b546a21434d906d8d21c2058fb3fdb0a58d67402
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-26 05:59:08 +0430

    REMOVE PURCHASE MODEL

--- app/models/purchase.rb
+++
@@ -1,7 +0,0 @@
-class Purchase < ApplicationRecord
-    belongs_to :item
-
-    validates :item_id, presence: true
-    validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-    validates :timestamp, presence: true
-end
