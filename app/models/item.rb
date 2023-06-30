5ebe5eb8f5c488029cc40a48ddb78314a8dd055a
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-06-30 10:15:16 +0430

    Make name and category required

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :list
+  belongs_to :list, optional: true
   belongs_to :category
 
-  validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :category_id, presence: true
-  validates :list_id, presence: true
-  validates :note, length: { maximum: 255 }
-  validates :image, length: { maximum: 255 }
 end
