4750c5ab0aa3a0a389252098e2cdbbceb7fe61f2
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-07-01 10:16:02 +0430

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  get 'items/top_item', to: 'items#top_item'
+  get 'items/top_categories', to: 'items#top_categories'
+  get 'lists/deactivate', to: 'lists#deactivate'
+
   resources :users
-    resources :lists, only: [:index, :show, :create, :update, :destroy] do
-      resources :items, only: [:index, :show, :create, :update, :destroy] do
-        collection do
-          get :top_items
-          get :top_categories
-        end
-      end
-    end
-  resources :categories, only: [:index, :create]
+    resources :lists, only: [:index, :show, :create, :update, :destroy, :deactivate] 
+
+  resources :categories, only: [:index, :create] do 
+    resources :items, only: [:index, :show, :create, :update, :destroy]
+  end 
 end
